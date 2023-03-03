19374e9b9ab53650ce85fa2e0416ef3d37e54021
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2023-03-03 11:33:28 +0000

    DTSPB-2794 fix build error with #57

---
+++ infrastructure/main.tf
@@ -0,0 +1,24 @@
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  vaultName = "${var.product}-${var.env}"
+}
+
+resource "azurerm_application_insights" "appinsights" {
+  name                = "${var.product}-${var.component}-appinsights-${var.env}"
+  location            = var.appinsights_location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
+
+  tags = var.common_tags
+
+  lifecycle {
+    ignore_changes = [
+      # Ignore changes to appinsights as otherwise upgrading to the Azure provider 2.x
+      # destroys and re-creates this appinsights instance
+      application_type,
+    ]
+  }
+}
