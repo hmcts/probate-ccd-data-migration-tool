c467d7c89cf7daf21abfd3ff681bea93c474abd6
Marcus Ho <99657446+MarcusHMCTS@users.noreply.github.com>
2023-03-09 17:04:36 +0000

    Update app-insights.tf

--- infrastructure/app-insights.tf
+++ infrastructure/app-insights.tf
@@ -4,7 +4,7 @@ data "azurerm_key_vault" "kv" {
 }
 
 resource "azurerm_key_vault_secret" "app_insights_connection_string" {
-  name         = "appinsights.connection_string"
+  name         = "app-insights-connection-string"
   value        = azurerm_application_insights.appinsights.connection_string
   key_vault_id = data.azurerm_key_vault.kv.id
 }
