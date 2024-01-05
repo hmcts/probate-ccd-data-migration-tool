e907bbd208d1b486f02daab46a8b69195e217b52
Chirag Pareek <chirag.pareek@justice.gov.uk>
2024-01-05 10:28:01 +0000

    DTSPO-15909 - migrated Application Insights

--- infrastructure/app-insights.tf
+++ infrastructure/app-insights.tf
@@ -5,6 +5,6 @@ data "azurerm_key_vault" "kv" {
 
 resource "azurerm_key_vault_secret" "app_insights_connection_string" {
   name         = "app-insights-connection-string"
-  value        = azurerm_application_insights.appinsights.connection_string
+  value        = module.application_insights.connection_string
   key_vault_id = data.azurerm_key_vault.kv.id
 }
