8801c14ad7cc742711e65906d0aec46c9cf2a92f
IswaryaPepakayala <Iswarya.Pepakayala@justice.gov.uk>
2024-01-05 15:27:01 +0000

    DTSPB-3926 Review comments

--- src/main/java/uk/gov/hmcts/reform/migration/ccd/CoreCaseDataService.java
+++ src/main/java/uk/gov/hmcts/reform/migration/ccd/CoreCaseDataService.java
@@ -93,7 +93,7 @@ public class CoreCaseDataService {
 
         CaseDetails updatedCaseDetails = startEventResponse.getCaseDetails();
 
-        if (updatedCaseDetails.getData().containsKey("caseHandedOffToLegacySite")) {
+        if (updatedCaseDetails.getData().containsKey("applicantOrganisationPolicy")) {
             CaseDataContent caseDataContent = CaseDataContent.builder()
                 .eventToken(startEventResponse.getToken())
                 .event(
