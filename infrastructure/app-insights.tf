772b632d15b9f0914354163c6fa5eb1d71956355
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2024-04-12 13:06:20 +0100

    DTSPB-4129 Fix appInsight Connection String

--- infrastructure/app-insights.tf
+++ infrastructure/app-insights.tf
@@ -4,7 +4,7 @@ data "azurerm_key_vault" "kv" {
 }
 
 resource "azurerm_key_vault_secret" "app_insights_connection_string" {
-  name         = "app-insights-connection-string-dmt"
+  name         = "app-insights-connection-string"
   value        = module.application_insights.connection_string
   key_vault_id = data.azurerm_key_vault.kv.id
 }
