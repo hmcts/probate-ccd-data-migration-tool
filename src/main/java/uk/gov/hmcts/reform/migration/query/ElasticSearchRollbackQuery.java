974fbd667b973220db6244aa261a9bf4eb087baf
jamiealbertelli <james.albertelli@justice.gov.uk>
2024-04-15 15:30:08 +0100

    Add source obtaining only the case number and value of paperForm to rollback

--- src/main/java/uk/gov/hmcts/reform/migration/query/ElasticSearchRollbackQuery.java
+++ src/main/java/uk/gov/hmcts/reform/migration/query/ElasticSearchRollbackQuery.java
@@ -16,6 +16,7 @@ public class ElasticSearchRollbackQuery {
                             }
                         }
                     ],
+                    "_source": ["reference", "data.paperForm"],
                 },
                 "size": %s,
                 "sort": [
