18c07043b2346e98722781c31ccd934e895f376b
jamiealbertelli <james.albertelli@justice.gov.uk>
2024-07-16 11:11:46 +0100

    DTSPB-4097: Add logic for appSubDate migration/rollback

--- src/main/java/uk/gov/hmcts/reform/migration/ccd/CoreCaseDataService.java
+++ src/main/java/uk/gov/hmcts/reform/migration/ccd/CoreCaseDataService.java
@@ -49,7 +49,7 @@ public class CoreCaseDataService {
 
         CaseDetails updatedCaseDetails = startEventResponse.getCaseDetails();
 
-        if (!updatedCaseDetails.getData().containsKey("applicationSubmittedDate")) {
+        if (updatedCaseDetails.getData().get("applicationSubmittedDate") == null) {
             CaseDataContent caseDataContent = CaseDataContent.builder()
                 .eventToken(startEventResponse.getToken())
                 .event(
@@ -93,7 +93,7 @@ public class CoreCaseDataService {
 
         CaseDetails updatedCaseDetails = startEventResponse.getCaseDetails();
 
-        if (updatedCaseDetails.getData().containsKey("applicationSubmittedDate")) {
+        if (updatedCaseDetails.getData().get("applicationSubmittedDate") == null) {
             CaseDataContent caseDataContent = CaseDataContent.builder()
                 .eventToken(startEventResponse.getToken())
                 .event(
