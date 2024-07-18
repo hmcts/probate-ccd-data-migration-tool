6ea2347d986cbcf689dcc1ae9e37f566cc6c7414
jamiealbertelli <james.albertelli@justice.gov.uk>
2024-07-18 16:36:09 +0100

    DTSPB-4097: Add correct condition check

--- src/main/java/uk/gov/hmcts/reform/migration/ccd/CoreCaseDataService.java
+++ src/main/java/uk/gov/hmcts/reform/migration/ccd/CoreCaseDataService.java
@@ -93,7 +93,7 @@ public class CoreCaseDataService {
 
         CaseDetails updatedCaseDetails = startEventResponse.getCaseDetails();
 
-        if (updatedCaseDetails.getData().get("applicationSubmittedDate") == null) {
+        if (updatedCaseDetails.getData().get("applicationSubmittedDate") != null) {
             CaseDataContent caseDataContent = CaseDataContent.builder()
                 .eventToken(startEventResponse.getToken())
                 .event(
