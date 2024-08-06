dff3ea6645d2e1b3016edfff754794fde3e974ea
FeliTam <feli.tam@justice.gov.uk>
2024-08-06 19:00:14 +0100

    DTSPB-4285 migrate applicationSubmittedDate

--- src/main/java/uk/gov/hmcts/reform/migration/service/AuditEventService.java
+++ src/main/java/uk/gov/hmcts/reform/migration/service/AuditEventService.java
@@ -17,13 +17,13 @@ import java.util.Optional;
 public class AuditEventService {
     private final CaseDataApiV2 caseDataApi;
 
-    public Optional<AuditEvent> getLatestAuditEventByName(String caseId, List<String> eventName,
+    public Optional<AuditEvent> getCaseCreationAuditEventByName(String caseId, List<String> eventName,
                                                           String userToken, String authToken) {
         AuditEventsResponse auditEventsResponse
             = caseDataApi.getAuditEvents(userToken, authToken, false, caseId);
 
         return auditEventsResponse.getAuditEvents().stream()
             .filter(auditEvent -> eventName.contains(auditEvent.getId()))
-            .max(Comparator.comparing(AuditEvent::getCreatedDate));
+            .min(Comparator.comparing(AuditEvent::getCreatedDate));
     }
 }
