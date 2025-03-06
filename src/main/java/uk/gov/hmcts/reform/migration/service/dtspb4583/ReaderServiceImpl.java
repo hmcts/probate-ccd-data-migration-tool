ae6c71d30a84b966b752ddbec39bba6d84fa33af
Tom Saunders <tom.saunders@justice.gov.uk>
2025-03-06 13:48:11 +0000

    Use correct method to load classpath resource.

--- src/main/java/uk/gov/hmcts/reform/migration/service/dtspb4583/ReaderServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/migration/service/dtspb4583/ReaderServiceImpl.java
@@ -1,19 +1,19 @@
 package uk.gov.hmcts.reform.migration.service.dtspb4583;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ResourceUtils;
 
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.Reader;
 
 @Service
 class ReaderServiceImpl implements ReaderService {
     @Override
     public Reader getFileReader(String fileName) throws IOException {
-        final File dtspb4853File =
-            ResourceUtils.getFile(ResourceUtils.CLASSPATH_URL_PREFIX + "DTSPB-4583.csv");
-        return new FileReader(dtspb4853File);
+        final ClassPathResource dtspbCsv = new ClassPathResource(fileName);
+        final InputStream inputStream = dtspbCsv.getInputStream();
+        return new InputStreamReader(inputStream);
     }
 }
