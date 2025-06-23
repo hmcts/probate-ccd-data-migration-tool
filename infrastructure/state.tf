73be1f408527aa1e0e99ee467d275f1f5cdd677f
Feli Tam <feli.tam@justice.gov.uk>
2025-06-23 15:56:52 +0100

    DTSPB-4854 update renovate, state.tf (#182)

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -3,8 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.25"
-      version = "~> 4.0"
+      version = "~> 4.33.0"
     }
   }
 }
