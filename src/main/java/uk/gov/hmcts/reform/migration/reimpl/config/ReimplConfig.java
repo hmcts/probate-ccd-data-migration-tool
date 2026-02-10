1b9735d70ddbebf67740ba6998f31181ae53aac1
Jamie Albertelli <James.Albertelli@justice.gov.uk>
2026-02-10 17:03:25 +0000

    DTSPB-5064: Update config with 5064 handlers

--- src/main/java/uk/gov/hmcts/reform/migration/reimpl/config/ReimplConfig.java
+++ src/main/java/uk/gov/hmcts/reform/migration/reimpl/config/ReimplConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import uk.gov.hmcts.reform.migration.reimpl.migrations.dtspb5005.Dtspb5005MigrationHandler;
 import uk.gov.hmcts.reform.migration.reimpl.migrations.dtspb5005.Dtspb5005RollbackMigrationHandler;
+import uk.gov.hmcts.reform.migration.reimpl.migrations.dtspb5064.Dtspb5064MigrationHandler;
+import uk.gov.hmcts.reform.migration.reimpl.migrations.dtspb5064.Dtspb5064RollbackMigrationHandler;
 import uk.gov.hmcts.reform.migration.reimpl.service.MigrationHandler;
 
 import java.time.Clock;
@@ -62,11 +64,16 @@ public class ReimplConfig {
     @Bean
     public Map<String, MigrationHandler> migrationHandlers(
         final Dtspb5005MigrationHandler dtspb5005MigrationHandler,
-        final Dtspb5005RollbackMigrationHandler dtspb5005RollbackMigrationHandler
+        final Dtspb5005RollbackMigrationHandler dtspb5005RollbackMigrationHandler,
+        final Dtspb5064MigrationHandler dtspb5064MigrationHandler,
+        final Dtspb5064RollbackMigrationHandler dtspb5064RollbackMigrationHandler
+
     ) {
         return Map.of(
             "DTSPB-5005", dtspb5005MigrationHandler,
-           "DTSPB-5005_rollback", dtspb5005RollbackMigrationHandler);
+            "DTSPB-5005_rollback", dtspb5005RollbackMigrationHandler,
+            "DTSPB-5064", dtspb5064MigrationHandler,
+            "DTSPB-5064_rollback", dtspb5064RollbackMigrationHandler);
     }
 
     @Bean
